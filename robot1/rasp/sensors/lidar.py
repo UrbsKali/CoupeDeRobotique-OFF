fce45d9a4f95089024aaa53f53ffe49cf1758312
Florian-BARRE <flo.others@gmail.com>
2024-04-15 16:19:50 +0200

    lidar -> raise an error when disconnected

--- robot1/rasp/sensors/lidar.py
+++ robot1/rasp/sensors/lidar.py
@@ -27,14 +27,14 @@ class Lidar:
     """
 
     def __init__(
-        self,
-        logger: Logger,
-        min_angle: float,
-        max_angle: float,
-        unit_angle: str = "deg",
-        unit_distance: str = "cm",
-        min_distance: float = 5.0,
-        initialization_fail_refresh_rate: float = 0.5,
+            self,
+            logger: Logger,
+            min_angle: float,
+            max_angle: float,
+            unit_angle: str = "deg",
+            unit_distance: str = "cm",
+            min_distance: float = 5.0,
+            initialization_fail_refresh_rate: float = 0.5,
     ) -> None:
         """
         Initialize the lidar object and the polars angles.
@@ -210,6 +210,8 @@ class Lidar:
                 self.__is_connected = False
                 self.__threading_init_lidar()
 
+            raise Exception(f"Error while scanning, LiDAR is disconnected ? [{error}]")
+
     """
         Public methods and properties
     """
@@ -227,8 +229,8 @@ class Lidar:
         :return: the distances array
         """
         return (
-            np.array(self.__lidar_obj.scan.distances, dtype=np.float32)
-            * self.__distance_unit
+                np.array(self.__lidar_obj.scan.distances, dtype=np.float32)
+                * self.__distance_unit
         )
 
     @property
