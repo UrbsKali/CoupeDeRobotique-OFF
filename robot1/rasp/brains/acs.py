c4cc58343452c91ac56d1df62dc93fff6f7b2190
AdrienB <56419448+DaAdri@users.noreply.github.com>
2024-04-18 00:08:33 +0200

    Fix

--- robot1/rasp/brains/acs.py
+++ robot1/rasp/brains/acs.py
@@ -2,15 +2,16 @@ from enum import Enum
 
 
 class AntiCollisionMode(Enum):
-    DISABLED = 0  # No anti-collision not implemented
-    CIRCULAR = 1  # Stop when an obstacle is detected not implemented
-    FRONTAL = (
-        3  # Stop when an obstacle is detected in front of the robot not implemented
-    )
+    # No anti-collision not implemented
+    DISABLED = 0
+    # Stop when an obstacle is detected not implemented
+    CIRCULAR = 1
+    # Stop when an obstacle is detected in front of the robot not implemented
+    FRONTAL = 2
 
 
 class AntiCollisionHandle(Enum):
-    CHANGE_DIRECTION = (
-        0  # Change direction when an obstacle is detected not implemented
-    )
-    AVOID = 1  # Avoid the obstacle not implemented
+    # Change direction when an obstacle is detected not implemented
+    CHANGE_DIRECTION = 0
+    # Avoid the obstacle not implemented
+    AVOID = 1
