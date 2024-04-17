760cb0f821a42e5ad6a8f9ad253d6e0de7c54338
Florian-BARRE <flo.others@gmail.com>
2024-04-18 01:45:03 +0200

    fix map WebUI

--- server/WebUI/page/map.js
+++ server/WebUI/page/map.js
@@ -1,5 +1,13 @@
 let wsm = new WebSocketManager();
 wsm.add_ws("odometer");
+wsm.add_handler("odometer", (event) => {
+        data = JSON.parse(event.data);
+        [x, y, theta] = 0, 0, 0
+        parse_pos()
+        set_rob_pos()
+    }
+)
+
 let rob = document.querySelector(".rob")
 
 let x = 0
@@ -10,14 +18,6 @@ let theta = 0
 // Dimension en mètre : 3 x 2 m
 // en X : 1 m = 496.33 px | 1cm = 4.96 px
 // en Y : 1 m = 500 px    | 1cm = 5 px
-
-position_ws.onmessage = function (event) {
-    data = JSON.parse(event.data);
-    [x, y, theta] = 0, 0, 0
-    parse_pos()
-    set_rob_pos()
-}
-
 function parse_pos() {
     x = Math.min(parseFloat(x) * 4.9633, 300 * 4.9633)
     y = Math.min(parseFloat(y) * 5.0, 200 * 5)
@@ -27,8 +27,4 @@ function parse_pos() {
 function set_rob_pos() {
     rob.style.transformOrigin = "top left"
     rob.style.transform = `translate(${x}px, ${y}px) rotate(${theta}deg)`
-}
-
-position_ws.onopen = function () {
-    console.log("map connected");
 }
