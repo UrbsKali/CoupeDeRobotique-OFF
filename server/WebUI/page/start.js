5911371b12540d0bc1f355705ed0fcd907599933
Florian-BARRE <flo.others@gmail.com>
2024-04-18 04:02:53 +0200

    fix map WebUI

--- server/WebUI/page/start.js
+++ server/WebUI/page/start.js
@@ -7,17 +7,38 @@ let yellow = document.querySelectorAll(".yellow");
 let blue_pos = [[380, 30], [1670, 430], [380, 820]];
 let yellow_pos = [[1670, 30], [380, 430], [1670, 820]];
 
+yellow[0].addEventListener("click", function (event) {
+    console.log("click on yellow n°" + 2);
+    wsm.send("cmd", "zone", 2);
+});
+
+yellow[1].addEventListener("click", function (event) {
+    console.log("click on yellow n°" + 4);
+    wsm.send("cmd", "zone", 4);
+});
+
+yellow[2].addEventListener("click", function (event) {
+    console.log("click on yellow n°" + 0);
+    wsm.send("cmd", "zone", 0);
+});
+
+
+blue[0].addEventListener("click", function (event) {
+    console.log("click on blue n°" + 1);
+    wsm.send("cmd", "zone", 1);
+});
+
+blue[1].addEventListener("click", function (event) {
+    console.log("click on blue n°" + 3);
+    wsm.send("cmd", "zone", 3);
+});
+
+blue[2].addEventListener("click", function (event) {
+    console.log("click on blue n°" + 5);
+    wsm.send("cmd", "zone", 5);
+});
+
 for (let i = 0; i < blue_pos.length; i++) {
-
-    blue[i].addEventListener("click", function (event) {
-        console.log("click on blue n°" + i);
-        wsm.send("cmd", "zone", i);
-    });
-    yellow[i].addEventListener("click", function (event) {
-        console.log("click on yellow n°" + i);
-        wsm.send("cmd", "zone", i+3);
-    });
-
     // set the initial position
     blue[i].style.left = blue_pos[i][0] + "px";
     blue[i].style.top = blue_pos[i][1] + "px";
