f37efa3a56beb20b88f98148abbb65e3211a7278
UrbsKali <urbain.lantres@tutanota.com>
2024-04-18 14:17:03 +0200

    Working rt map

--- server/WebUI/page/map.js
+++ server/WebUI/page/map.js
@@ -3,8 +3,8 @@ wsm.add_ws("odometer");
 wsm.add_handler("odometer", (event) => {
     console.log(event.data);
     let data = JSON.parse(event.data)["data"];
-    x = data[0];
-    y = data[1];
+    y = data[0];
+    x = data[1];
     theta = data[2];
     parse_pos();
     set_rob_pos();
@@ -17,7 +17,7 @@ let x = 0, y = 0, theta = 0;
 function parse_pos() {
     x = Math.min(parseFloat(x) * 4.896, 1469); // Max width in pixels, adjusted to actual width
     y = Math.min(parseFloat(y) * 4.9, 980); // Max height in pixels, adjusted to actual height
-    theta = parseFloat(theta) * 180 / Math.PI; // Convert radians to degrees
+    theta = parseFloat(theta) * 180 / Math.PI + 90; // Convert radians to degrees
 }
 
 function set_rob_pos() {
