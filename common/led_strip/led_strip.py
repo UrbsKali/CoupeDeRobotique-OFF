e71fb2f4174ef67b0115b0f84796eb95dd576f8e
Florian-BARRE <flo.others@gmail.com>
2024-05-03 11:35:45 +0200

    fix led

--- common/led_strip/led_strip.py
+++ common/led_strip/led_strip.py
@@ -6,6 +6,7 @@ LogColors = {
     LogLevels.INFO: Color(0, 0, 255),
     LogLevels.WARNING: Color(250, 250, 0),
     LogLevels.ERROR: Color(255, 0, 0),
+    LogLevels.CRITICAL: Color(255, 0, 0),
     LogLevels.FATAL: Color(255, 0, 0)
 }
 
