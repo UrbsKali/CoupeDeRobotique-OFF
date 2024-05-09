3106179ef0fb380e9dc6fb0444cb170301402a9a
AdrienB <56419448+DaAdri@users.noreply.github.com>
2024-05-09 12:06:10 +0200

    Fix

--- robot1/teensy_moteur/lib/speed_driver/src/speed_driver.cpp
+++ robot1/teensy_moteur/lib/speed_driver/src/speed_driver.cpp
@@ -12,7 +12,7 @@ void Speed_Driver::compute_acceleration_profile(Rolling_Basis_Params *rolling_ba
     {
         this->acceleration_params.a = this->acceleration_params.offset / this->max_speed;
         this->acceleration_params.b = this->acceleration_params.distance / 2;
-        this->acceleration_params.c = logf(((1 - this->acceleration_params.a) / this->acceleration_params.p) - 1) / this->acceleration_params.b;
+        this->acceleration_params.c = logf(((1 - this->acceleration_params.a) / this->acceleration_params.p) - 1.0f) / this->acceleration_params.b;
     }
 
     // Compute deceleration profile
@@ -20,7 +20,7 @@ void Speed_Driver::compute_acceleration_profile(Rolling_Basis_Params *rolling_ba
     {
         this->deceleration_params.a = this->deceleration_params.offset / this->max_speed;
         this->deceleration_params.b = this->deceleration_params.distance / 2;
-        this->deceleration_params.c = logf(((1 - this->deceleration_params.a) / this->deceleration_params.p) - 1) / this->deceleration_params.b;
+        this->deceleration_params.c = logf(((1 - this->deceleration_params.a) / this->deceleration_params.p) - 1.0f) / this->deceleration_params.b;
     }
 }
 
@@ -32,11 +32,11 @@ byte Speed_Driver::compute_local_speed(long ticks)
     // Compute speed with acceleration and deceleration curves
     byte acceleration_speed = this->max_speed;
     if (this->acceleration_params.distance != 0.0f)
-        acceleration_speed = (byte)constrain(this->max_speed * (1.0f - this->acceleration_params.a) * ((1.0f / (1.0f + expf(-this->acceleration_params.c * (ticks - this->acceleration_params.b)))) + this->acceleration_params.a), this->acceleration_params.offset, this->max_speed);
+        acceleration_speed = (byte)constrain(this->max_speed * (1.0f - this->acceleration_params.a) * ((1.0f / (1.0f + expf(-this->acceleration_params.c * (ticks - this->acceleration_params.b))))) + this->acceleration_params.a, this->acceleration_params.offset, this->max_speed);
 
     byte deceleration_speed = this->max_speed;
     if (this->deceleration_params.distance != 0.0f)
-        deceleration_speed = (byte)constrain(this->max_speed * (1.0f - this->deceleration_params.a) * ((1.0f / (1.0f + expf(-this->deceleration_params.c * (delta_ticks - this->deceleration_params.b)))) + this->deceleration_params.a), this->deceleration_params.offset, this->max_speed);
+        deceleration_speed = (byte)constrain(this->max_speed * (1.0f - this->deceleration_params.a) * ((1.0f / (1.0f + expf(-this->deceleration_params.c * (delta_ticks - this->deceleration_params.b))))) + this->deceleration_params.a, this->deceleration_params.offset, this->max_speed);
 
     // Keep min vale between both curves values
     if (acceleration_speed < deceleration_speed)
