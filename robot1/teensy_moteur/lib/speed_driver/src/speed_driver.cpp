732d79295d2d9fc33e4d76f1a0cb46aa0795d54f
AdrienB <56419448+DaAdri@users.noreply.github.com>
2024-05-09 06:11:42 +0200

    Format

--- robot1/teensy_moteur/lib/speed_driver/src/speed_driver.cpp
+++ robot1/teensy_moteur/lib/speed_driver/src/speed_driver.cpp
@@ -32,18 +32,17 @@ byte Speed_Driver::compute_local_speed(long ticks)
     // Compute speed with acceleration and deceleration curves
     byte acceleration_speed = this->max_speed;
     if (this->acceleration_params.distance != 0.0f)
-        acceleration_speed = (byte)(this->max_speed*(1.0f-this->acceleration_params.a)*((1.0f/(1.0f+expf(-this->acceleration_params.c*(ticks-this->acceleration_params.b))))+this->acceleration_params.a));
+        acceleration_speed = (byte)constrain(this->max_speed * (1.0f - this->acceleration_params.a) * ((1.0f / (1.0f + expf(-this->acceleration_params.c * (ticks - this->acceleration_params.b)))) + this->acceleration_params.a), this->acceleration_params.offset, this->max_speed);
 
     byte deceleration_speed = this->max_speed;
-    if(this->deceleration_params.distance != 0.0f)
-        deceleration_speed = (byte)(this->max_speed*(1.0f-this->deceleration_params.a)*((1.0f/(1.0f+expf(-this->deceleration_params.c*(delta_ticks-this->deceleration_params.b))))+this->deceleration_params.a));
+    if (this->deceleration_params.distance != 0.0f)
+        deceleration_speed = (byte)constrain(this->max_speed * (1.0f - this->deceleration_params.a) * ((1.0f / (1.0f + expf(-this->deceleration_params.c * (delta_ticks - this->deceleration_params.b)))) + this->deceleration_params.a), this->deceleration_params.offset, this->max_speed);
 
     // Keep min vale between both curves values
-    if(acceleration_speed < deceleration_speed)
+    if (acceleration_speed < deceleration_speed)
         return acceleration_speed;
     return deceleration_speed;
 }
-
 
 // Speed driver from distance
 Speed_Driver_From_Distance::Speed_Driver_From_Distance(byte max_speed, byte correction_speed, float acceleration_offset, float acceleration_distance, float deceleration_offset, float deceleration_distance)
@@ -54,12 +53,12 @@ Speed_Driver_From_Distance::Speed_Driver_From_Distance(byte max_speed, byte corr
     // Acceleration params
     this->acceleration_params.offset = acceleration_offset;
     this->acceleration_params.distance = acceleration_distance;
-    // Fix sigmoid power 
+    // Fix sigmoid power
     this->acceleration_params.p = 0.002f;
 
     // Deceleration params
     this->deceleration_params.offset = deceleration_offset;
     this->deceleration_params.distance = deceleration_distance;
-    // Fix sigmoid power 
+    // Fix sigmoid power
     this->deceleration_params.p = 0.002f;
 }
