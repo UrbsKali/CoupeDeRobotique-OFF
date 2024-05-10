0a5c77c74ecc479fd4f10ef1e67e0390e903045d
UrbsKali <urbain.lantres@tutanota.com>
2024-05-10 22:31:41 +0200

    yyyy

--- client/main.py
+++ client/main.py
@@ -1,5 +1,5 @@
 import multiprocessing
-
+from websockets.client import connect
 from pynput.keyboard import Key, Listener
 import time
 
@@ -8,26 +8,16 @@ power_r = 100
 
 power = 100
 
-def send_data_to_robot(data):
-    start_time = time.time()
-    process = multiprocessing.Process(target=thread_send_data, args=(data,))
-    process.start()
+# create ws
 
 
-def thread_send_data(data):
-    import asyncio
-
-    asyncio.run(send_data(data))
-
-
-async def send_data(data):
-    import websockets as ws
-    dat = '{ "msg":' + data + ', "type" : "eval", "sender" : "urbai"}'
+async def send_data(data, ws):
+    dat = '{ "data": "' + data + '", "msg" : "eval", "sender" : "urbai", "ts":"12334"}'
+    print(dat)
 
     try:
-        async with ws.connect(f"ws://rc.local:8080/cmd?sender=urbai", open_timeout=0.1) as websocket:
-            await websocket.send(dat, timeout=0.1)
-            print(f"Data sent: {dat}")
+        await ws.send(dat)
+        print(f"Data sent: {dat}")
     except Exception as e:
         print(f"Error while sending data: {e}")
 
@@ -35,69 +25,85 @@ async def send_data(data):
 def on_press(key):
     if key not in current_key:
         current_key.append(key)
-    else:
-        return
-
-    power_r = 200 if Key.shift in current_key else 100
-    power_l = 200 if Key.shift in current_key else 100
-    power = 255 if Key.shift in current_key else 100
-    
-    dir = [0,0]
-    
-        
-    
-    match key:
-        case Key.esc:
-            return False
-        case Key.up:
-            send_data_to_robot(f"self.robot.vroum({power}, True)")
-        case Key.down:
-            send_data_to_robot(f"self.robot.vroum({power}), False")
-        case Key.left:
-            send_data_to_robot(f"self.robot.rotate({power}, True)")
-        case Key.right:
-            send_data_to_robot(f"self.robot.rotate({power}, False)")
-        case 'o':
-            send_data_to_robot(f"self.robot.l_motor({power}, True)")
-        case 'p':
-            send_data_to_robot(f"self.robot.r_motor({power}, True)")
-        case 'l':
-            send_data_to_robot(f"self.robot.l_motor({power}, False)")
-        case 'm':
-            send_data_to_robot(f"self.robot.r_motor({power}, False)")
-        
-        case 'z':
-            send_data_to_robot(f"self.robot.l_motor({power_l}, True)")
-            send_data_to_robot(f"self.robot.r_motor({power_r}, True)")
-        case 'q':
-            send_data_to_robot(f"self.robot.l_motor({power_l}, True)")
-            send_data_to_robot(f"self.robot.r_motor({power_r+50}, True)")
-        case 's':
-            send_data_to_robot(f"self.robot.l_motor({power_l}, False)")
-            send_data_to_robot(f"self.robot.r_motor({power_r}, False)")
-        case 'd':
-            send_data_to_robot(f"self.robot.l_motor({power_l+50}, True)")
-            send_data_to_robot(f"self.robot.r_motor({power_r}, True)")
-            
+    if key == Key.esc:
+        return False
 
 def on_release(key):
     if key in current_key:
         current_key.remove(key)
-    match key:
-        case Key.esc:
-            return False
-        case Key.shift:
-            power_l = 100
-            power_r = 100
-            power = 100
-        case _:
-            send_data_to_robot("self.robot.stop()")
+    if key == Key.esc:
+        # Stop listener
+        return False
 
+
+
+async def mainloop():
+    websocket = await connect("ws://rc.local:8080/cmd?sender=urbai")
+    while True:
+        power_r = 200 if Key.shift in current_key else 100
+        power_l = 200 if Key.shift in current_key else 100
+        power = 255 if Key.shift in current_key else 100
+
+        dir = [0, 0]
+        
+        
+
+        for key in current_key:
+            if key not in old_current_key:
+                print(f"Key pressed: {key}")
+                old_current_key.append(key)
+            else: 
+                continue
+            match key:
+                case Key.esc:
+                    return False
+                case Key.up:
+                    await send_data(f"self.robot.vromm({power}, True)", websocket)
+                case Key.down:
+                    await send_data(f"self.robot.vromm({power}), False", websocket)
+                case Key.left:
+                    await send_data(f"self.robot.rotate({power}, True)", websocket)
+                case Key.right:
+                    await send_data(f"self.robot.rotate({power}, False)", websocket)
+                case "o":
+                    await send_data(f"self.robot.l_motor({power}, True)", websocket)
+                case "p":
+                    await send_data(f"self.robot.r_motor({power}, True)", websocket)
+                case "l":
+                    await send_data(f"self.robot.l_motor({power}, False)", websocket)
+                case "m":
+                    await send_data(f"self.robot.r_motor({power}, False)", websocket)
+
+                case "z":
+                    await send_data(f"self.robot.l_motor({power_l}, True)", websocket)
+                    await send_data(f"self.robot.r_motor({power_r}, True)", websocket)
+                case "q":
+                    await send_data(f"self.robot.l_motor({power_l}, True)", websocket)
+                    await send_data(f"self.robot.r_motor({power_r+50}, True)", websocket)
+                case "s":
+                    await send_data(f"self.robot.l_motor({power_l}, False)", websocket)
+                    await send_data(f"self.robot.r_motor({power_r}, False)", websocket)
+                case "d":
+                    await send_data(f"self.robot.l_motor({power_l+50}, True)", websocket)
+                    await send_data(f"self.robot.r_motor({power_r}, True)", websocket)
+                    
+                case Key.ctrl_l:
+                    await send_data(f"self.robot.stop()", websocket)
+        for key in old_current_key:
+            if key not in current_key:
+                print(f"Key released: {key}")
+                old_current_key.remove(key)
+                await send_data(f"self.robot.stop()", websocket)
 
 if __name__ == "__main__":
+    import asyncio
     # Import from common
     current_key = []
+    old_current_key = []
     print("Press ESC to stop.")
     # Collect events until released
     with Listener(on_press=on_press, on_release=on_release) as listener:
+        asyncio.run(mainloop())
         listener.join()
+
+
